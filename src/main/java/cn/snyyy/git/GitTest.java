ffe2a3ddf7b54b9fc57d45e8b792dff8c41b235a
delyn_pro <snsdyrmyyxxk@163.com>
2021-12-04 00:23:03 +0800

    second commit

--- src/main/java/cn/snyyy/git/GitTest.java
+++ src/main/java/cn/snyyy/git/GitTest.java
@@ -7,5 +7,6 @@ package cn.snyyy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
