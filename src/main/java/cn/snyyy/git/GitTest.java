8a12895c5f29a5ce6a18cc0d24794433b9661428
delyn_pro <snsdyrmyyxxk@163.com>
2021-12-04 00:19:06 +0800

    first commit

---
+++ src/main/java/cn/snyyy/git/GitTest.java
@@ -0,0 +1,11 @@
+package cn.snyyy.git;
+
+/**
+ * @author delin
+ * @create 2021-12-04 0:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
